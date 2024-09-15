6744f4b80b57eaf2af94d66159ebaaf956447a8b
HenriqueMLDEV <hmeloleal@gmail.com>
2024-09-15 00:17:57 -0300

    Metas Abertas

--- index.js
+++ index.js
@@ -57,8 +57,24 @@ const metasRealizadas = async () => {
   }
 
   await select({
-    message: "Metas Realizadas",
+    message: "Metas Realizadas" + realizadas.length,
     choices: [...realizadas],
+  });
+};
+
+const metasAbertas = async () => {
+  const abertas = metas.filter((meta) => {
+    return meta.checked != true;
+  });
+
+  if (abertas.length == 0) {
+    console.log("Não existem metas abertas :)");
+    return;
+  }
+
+  await select({
+    message: "Metas Abertas" + abertas.length,
+    choices: [...abertas],
   });
 };
 
@@ -80,6 +96,10 @@ const start = async () => {
           value: "Realizadas",
         },
         {
+          name: "Metas Abertas",
+          value: "Abertas",
+        },
+        {
           name: "Sair",
           value: "Sair",
         },
@@ -95,6 +115,9 @@ const start = async () => {
         break;
       case "Realizadas":
         await metasRealizadas();
+        break;
+      case "Abertas":
+        await metasAbertas();
         break;
       case "Sair":
         console.log("Até a próxima");
